d96945fbea633ad39c2dadb6ec7bf7b0beaf9004
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-12-24 18:45:00 +0530

    fixed the profile update bug

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://blog-website-dtgf.onrender.com"],
+    origin: '*',
     credentials: true,
   })
 );
