456bdeec71747f3e7de4815ae90e28b4b90ed170
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-12-24 21:23:18 +0530

    fixed the profile update bug

--- react/src/slices/apiSlice.js
+++ react/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://blog-website-dtgf.onrender.com",
 });
 
 export const apiSlice = createApi({
