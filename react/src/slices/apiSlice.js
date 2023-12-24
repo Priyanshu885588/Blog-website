e53c6dec07a6e6ff84515204971ff542838d02c3
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-12-24 13:09:47 +0530

    update profile

--- react/src/slices/apiSlice.js
+++ react/src/slices/apiSlice.js
@@ -1,9 +1,12 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({baseUrl:'https://blog-website-dtgf.onrender.com'})
+export const baseQuery = fetchBaseQuery({
+  baseUrl: "https://blog-website-dtgf.onrender.com",
+  credentials: "include",
+});
 
 export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['User'],
-    endpoints: (builder) => ({})
-})
+  baseQuery,
+  tagTypes: ["User"],
+  endpoints: (builder) => ({}),
+});
--- react/src/slices/usersApiSlice.js
+++ react/src/slices/usersApiSlice.js
@@ -1,30 +1,31 @@
 import { apiSlice } from "./apiSlice";
 const USERS_URL = "/api/users";
+import { baseQuery } from "./apiSlice";
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: (data) => ({
+      query: (data) =>baseQuery ({
         url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
-      query: (data) => ({
+      query: (data) =>baseQuery ({
         url: `${USERS_URL}`,
         method: "POST",
         body: data,
       }),
     }),
     logout: builder.mutation({
-      query: () => ({
+      query: () =>baseQuery ({
         url: `${USERS_URL}/logout`,
         method: "POST",
       }),
     }),
     updateUser: builder.mutation({
-      query: (data) => ({
+      query: (data) => baseQuery({
         url: `${USERS_URL}/profile`,
         method: "PUT",
         headers: {
