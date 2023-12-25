3ae424f99606e38c9eaa67bb214199cd20700db1
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-12-25 19:30:04 +0530

    added like count

--- backend/contollers/posts.js
+++ backend/contollers/posts.js
@@ -69,10 +69,36 @@ const deletePost = async (req, res) => {
   }
 };
 
+const toggleLike = async (req, res) => {
+  try {
+    const { postId } = req.body;
+    const { userId } = req.body;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    const userIndex = post.likes.indexOf(userId);
+
+    if (userIndex === -1) {
+      post.likes.push(userId);
+    } else {
+      post.likes.splice(userIndex, 1);
+    }
+    await post.save();
+    res
+      .status(200)
+      .json({ msg: "Like toggled successfully", likes: post.likes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
+
 module.exports = {
   getAllPosts,
   getSinglePost,
   createPost,
   updatePost,
   deletePost,
+  toggleLike,
 };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const router = express.Router();
-
 const {
     getAllPosts,
     getSinglePost,
     createPost,
     updatePost,
-    deletePost
+    deletePost,
+    toggleLike
 }=require('../contollers/posts')
+const protect = require("../middleware/authMiddleware");
 
 router.route("/").get(getAllPosts).post(createPost);
 router.route("/:id").get(getSinglePost).patch(updatePost).delete(deletePost);
+router.route("/:id/like").patch(protect,toggleLike);
 
 module.exports = router;
