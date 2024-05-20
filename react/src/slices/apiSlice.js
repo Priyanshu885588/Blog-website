ace22e1decde99e963ff603460884011bc1d3e31
Priyanshu885588 <priyanshumandani978@gmail.com>
2024-05-20 20:47:20 +0530

    updated the new deployment server api's

--- react/src/slices/apiSlice.js
+++ react/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://blog-website-dtgf.onrender.com",
+  baseUrl: "https://blog-website-6gjl.vercel.app",
 });
 
 export const apiSlice = createApi({
